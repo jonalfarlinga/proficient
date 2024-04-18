b83ad356c10ac764115dd84f4fdfc7db0a6b694e
Denny Bucklin <dennis.bucklin@gmail.com>
2024-04-18 17:05:57 -0500

    redux store

---
+++ ghi/src/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+export const store = configureStore({
+    reducer: {
+        
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat()
+})
+
+setupListeners(store.dispatch)
