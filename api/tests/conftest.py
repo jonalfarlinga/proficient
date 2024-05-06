a849789d54bff948f48044eaab4af87fd335b77f
Denny Bucklin <dennis.bucklin@gmail.com>
2024-05-06 15:29:19 -0500

    ready to use in-mem db

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,15 +1,36 @@
-import os
 import pytest
+import testing.postgresql
 from psycopg_pool import ConnectionPool
+
+factory = testing.postgresql.PostgresqlFactory(
+    cache_initialized_db=True
+)
 
 
 @pytest.fixture(autouse=True)
 def database_connection():
-    # Check if running tests
+    # Use factory to create db
+    postgresql = factory()
 
-    # Connect to the in-memory database
-    pool = ConnectionPool(
-        open=True,
-        conninfo="postgresql://test:test@localhost:5432/test")
+    # parse connection string
+    dsn = postgresql.dsn()
+    conn_string = f"postgresql://{dsn['user']}@{dsn['host']}:{dsn['port']}/{dsn['database']}"
+
+    # connect to db and create users table
+    pool = ConnectionPool(open=True, conninfo=conn_string)
+    with pool.connection() as conn:
+        with conn.cursor() as cursor:
+            # Create the users table
+            cursor.execute("""
+                CREATE TABLE users (
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    email VARCHAR(254) NOT NULL UNIQUE,
+                    username VARCHAR(100) NOT NULL UNIQUE,
+                    name VARCHAR(100) NOT NULL,
+                    password VARCHAR(255) NOT NULL UNIQUE
+                )
+            """)
 
     yield pool
+
+    postgresql.stop()
--- api/tests/test_conn.py
+++ api/tests/test_conn.py
@@ -1,2 +1,44 @@
-def test_conn(database_connection):
-    assert True
+from unittest.mock import patch
+from queries.users import UsersRepo
+from models import UserIn
+
+
+def test_create_user_adds_to_database(database_connection):
+    # Instantiate UsersRepo
+    users_repo = UsersRepo()
+    user_data = UserIn(
+        email="test@example.com",
+        username="testuser",
+        name="Test User",
+        password="password123"
+    )
+
+    # Create a user
+    with patch("queries.users.pool", new=database_connection):
+        created_user = users_repo.create_user(user_data)
+
+    # Assert that the user is created successfully
+    assert created_user is not None
+
+    # Fetch the user from the database
+    with database_connection.connection() as conn:
+        with conn.cursor() as db:
+            db.execute(
+                """
+                SELECT * FROM users
+                WHERE id = %s
+                """,
+                (created_user.id,)
+            )
+            fetched_users = db.fetchall()
+
+    for row in fetched_users:
+        if row[0] == created_user.id:
+            fetched_user = row
+
+    # Assert that the user is in the database and its details match
+    assert fetched_users is not None
+    assert fetched_user[0] == created_user.id
+    assert fetched_user[1] == user_data.email
+    assert fetched_user[2] == user_data.username
+    assert fetched_user[3] == user_data.name
